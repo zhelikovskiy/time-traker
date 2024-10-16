3aed42116cc4146960743d330709e76734aa4656
Artem Zhelikovskij <zhelikovskiy.artem@gmail.com>
2024-10-16 18:23:12 +0200

    Add webpack for renderer scripts

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-require-imports */
+const path = require('path');
+
+module.exports = {
+	entry: './src/renderer/index.ts',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	mode: 'development',
+};

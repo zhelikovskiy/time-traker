84c389a2ca2c43ea51589029f18014da154806f7
Artem Zhelikovskij <zhelikovskiy.artem@gmail.com>
2024-10-18 18:32:49 +0200

    Fix date format data

--- src/renderer/components/create-task-form.ts
+++ src/renderer/components/create-task-form.ts
@@ -2,17 +2,24 @@ const handleCreateTaskForm = (event: Event): void => {
 	event.preventDefault();
 	const form = document.getElementById('create-task-form') as HTMLFormElement;
 
+	const startDateElement = form.elements.namedItem(
+		'task-start-date'
+	) as HTMLInputElement;
+	const endDateElement = form.elements.namedItem(
+		'task-end-date'
+	) as HTMLInputElement;
+
 	const task = {
 		name: (form.elements.namedItem('task-name') as HTMLInputElement).value,
 
 		description: (form.elements.namedItem('task-desc') as HTMLTextAreaElement)
 			.value,
 
-		startDate: (form.elements.namedItem('task-start-date') as HTMLInputElement)
-			.value,
+		startDate: startDateElement.value
+			? new Date(startDateElement.value)
+			: new Date(),
 
-		endDate: (form.elements.namedItem('task-end-date') as HTMLInputElement)
-			.value,
+		endDate: endDateElement.value ? new Date(endDateElement.value) : undefined,
 	};
 
 	window.ipc.CreateTask(task);

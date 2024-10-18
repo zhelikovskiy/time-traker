faeed90bae09369a703eb3cf388a1edb1df28e91
Artem Zhelikovskij <zhelikovskiy.artem@gmail.com>
2024-10-18 17:44:18 +0200

    Implement Vue.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,32 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-require-imports */
 const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
 	entry: './src/renderer/index.ts',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
+		publicPath: 'dist/',
 	},
 	resolve: {
-		extensions: ['.ts', '.js'],
+		extensions: ['.ts', '.js', '.vue', '.json'],
 	},
 	module: {
 		rules: [
 			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+			},
+			{
 				test: /\.ts$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
+				loader: 'ts-loader',
+				options: { appendTsSuffixTo: [/\.vue$/] },
 			},
 		],
 	},
+	plugins: [new VueLoaderPlugin()],
+	target: 'electron-renderer',
 	mode: 'development',
 };

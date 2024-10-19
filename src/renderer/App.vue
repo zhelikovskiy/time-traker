0042d33b5b928129011715fee758eb9fe7cc0531
Artem Zhelikovskij <zhelikovskiy.artem@gmail.com>
2024-10-19 17:11:36 +0200

    Refactor some files

---
+++ src/renderer/App.vue
@@ -0,0 +1,5 @@
+<template>
+	<div id="app">
+		<p>Hello</p>
+	</div>
+</template>
--- src/renderer/index.ts
+++
@@ -1,8 +0,0 @@
-import handleCreateTaskForm from './components/create-task-form';
-import taskList from './components/task-list';
-
-window.ipc.onNewTaskCreated(taskList.handleAddNewTask);
-
-document
-	.getElementById('create-task-form')
-	?.addEventListener('submit', handleCreateTaskForm);

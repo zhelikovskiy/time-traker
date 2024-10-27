ff53415a8ac9b64a9bfdc56c004ac9b4e3380cdd
Artem Zhelikovskij <zhelikovskiy.artem@gmail.com>
2024-10-27 14:15:25 +0100

    Update eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import prettier from 'eslint-plugin-prettier';
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const prettier = require('eslint-plugin-prettier');
 
 export default tseslint.config(
 	eslint.configs.recommended,

1140854099d506124d65e1349ab644a82e0d141c
Artem Zhelikovskij <zhelikovskiy.artem@gmail.com>
2024-11-09 18:24:28 +0100

    Update Task-form

--- src/renderer/components/Task-Form.vue
+++ src/renderer/components/Task-Form.vue
@@ -1,44 +1,46 @@
 <template>
-	<form @submit.prevent="submitForm">
-		<div>
-			<label for="name">Name:</label>
-			<input v-model="task.name" type="text" id="name" name="name" required />
-		</div>
-		<div>
-			<label for="description">Description:</label>
-			<input
-				v-model="task.description"
-				type="text"
-				id="description"
-				name="description"
-			/>
-		</div>
-		<div>
-			<label for="startDate">Start Date:</label>
-			<input
-				v-model="task.startDate"
-				type="date"
-				id="startDate"
-				name="startDate"
-				required
-			/>
-		</div>
-		<div>
-			<label for="endDate">End Date:</label>
-			<input v-model="task.endDate" type="date" id="endDate" name="endDate" />
-		</div>
-		<div>
-			<label for="status">Status:</label>
-			<select id="status" v-model="task.status" required>
-				<option value="pending">Pending</option>
+	<div>
+		<h2>{{ id ? 'Edit task' : 'Create task' }}</h2>
 
-				<option value="in-progress">In Progress</option>
+		<form @submit.prevent="submit">
+			<div>
+				<label for="name">Name:</label>
+				<input type="text" id="name" v-model="task.name" />
+			</div>
 
-				<option value="completed">Completed</option>
-			</select>
-		</div>
-		<button type="submit">Create Task</button>
-	</form>
+			<div>
+				<label for="description">Description:</label>
+				<textarea id="description" v-model="task.description"></textarea>
+			</div>
+
+			<div>
+				<label for="startDate">Start Date:</label>
+				<input
+					type="datetime-local"
+					id="startDate"
+					v-model="task.startDate"
+					:min="new Date().toISOString().slice(0, 16)"
+					required
+				/>
+			</div>
+
+			<div>
+				<label for="endDate">End Date:</label>
+				<input type="date" id="endDate" v-model="task.endDate" />
+			</div>
+
+			<div v-if="id">
+				<label for="status">Status:</label>
+				<select id="status" v-model="task.status">
+					<option value="todo">To Do</option>
+					<option value="doing">Doing</option>
+					<option value="done">Done</option>
+				</select>
+			</div>
+
+			<button type="submit">{{ id ? 'Update' : 'Create' }}</button>
+		</form>
+	</div>
 </template>
 
 <script setup>
@@ -46,33 +48,53 @@ import { ref, defineProps, onMounted } from 'vue';
 
 const props = defineProps({
 	id: {
-		type: Object,
+		type: String,
 		required: false,
 	},
 });
 
-const task = ref({});
+const task = ref({
+	name: '',
+	description: '',
+	status: 'todo',
+	startDate: '',
+	endDate: null,
+});
 
 onMounted(async () => {
 	try {
 		if (props.id) {
-			console.log(task.value);
-			task.value = await window.ipc.getTaskInfo(props.id);
-		}
-	} catch (error) {}
-});
+			const response = await window.ipc.getTaskInfo(props.id);
 
-const submitForm = async () => {
-	try {
-		if (props.id) {
-			//await window.ipc.updateTask(props.id, task.value);
-			alert('Task updated successfully');
+			task.value = response;
+			if (response.endDate) task.value.endDate = toOnlyDate(response.endDate);
 		} else {
-			//await window.ipc.createTask(task.value);
-			alert('Task created successfully');
+			task.value.startDate = new Date().toISOString().slice(0, 16);
 		}
 	} catch (error) {
-		console.error('Error creating task:', error);
+		console.error('Error fetching task:', error);
+	}
+});
+
+const submit = async () => {
+	try {
+		if (props.id) {
+			console.log(task.value);
+		} else {
+			const taskData = {
+				name: task.value.name,
+				description: task.value.description,
+				status: task.value.status,
+				startDate: new Date(task.value.startDate),
+				endDate: task.value.endDate
+					? new Date(task.value.endDate)
+					: task.value.endDate,
+			};
+
+			await window.ipc.createTask(taskData);
+		}
+	} catch (error) {
+		console.error('Error saving task:', error);
 	}
 };
 </script>
